86b61eb97b0625ca8ae02f86181b93976b9d050d
Sethvin Nanayakkara <nanasethvin@gmail.com>
2024-12-19 19:41:17 -0600

    update Landing page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "41cd7355fa8ac86f"; }
+/******/ 		__webpack_require__.h = function() { return "ca5d41292baf3e45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
