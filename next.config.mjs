fcd0bf9ec9f4d05b28d99ce9312ecc15249f124f
Sethvin Nanayakkara <nanasethvin@gmail.com>
2025-02-06 02:29:47 -0600

    updated package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/Portfolio-site", // Ensures correct GitHub Pages routing
+  trailingSlash: true,         // Helps prevent 404 errors
+  distDir: "out", 
   reactStrictMode: true,
 };
+
 
 export default nextConfig;
