0dcbf7a75aa1d511842d277140e464bf5e9cacde
Sethvin Nanayakkara <nanasethvin@gmail.com>
2025-04-30 16:36:53 -0500

    updates 1

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const path = require('path');
 
 const nextConfig = {
-  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
