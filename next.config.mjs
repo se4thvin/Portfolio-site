d4e97102577f6421539674a5a8f57524039bd8c4
Estevan Guerra <estevanguerra8@gmail.com>
2025-04-29 21:47:05 -0500

    updated deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
  // Ensures correct GitHub Pages routing
   trailingSlash: true,         // Helps prevent 404 errors 
   reactStrictMode: true,
+  // Explicitly set the output directory
+  distDir: '.next',
 };
 
 
