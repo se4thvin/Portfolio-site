53d5cf2b75320188c4e43f0ee1cc50224f245b8e
Sethvin <nanasethvin@gmail.com>
2025-09-09 20:18:29 -0400

    chore: update Next.js build files and remove deprecated assets
    - Updated package-lock.json to reflect dependency changes
    - Cleaned up various build and server files, including removal of unused JSON and HTML files
    - Updated server and client chunk files for improved performance
    - Removed deprecated font and CSS files to streamline the project

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\User\Desktop\Portfolio\Portfolio-site\src\app\page.tsx
+// File: /Users/sethvin-nanayakkara/Desktop/Portfolio-site/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
